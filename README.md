8182b271ea6bba2e16d8845a7c7fd5c566d23fad
msjemenblc <msjemenblc@gmail.com>
2025-05-06 11:51:28 +0200

    [INIT]

---
+++ README.md
@@ -0,0 +1,1 @@
+"# P13 DirectMessage PoC" 
---
+++ back/src/main/java/com/yourcaryourway/dm_poc/DmPocApplication.java
@@ -0,0 +1,13 @@
+package com.yourcaryourway.dm_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmPocApplication.class, args);
+	}
+
+}
